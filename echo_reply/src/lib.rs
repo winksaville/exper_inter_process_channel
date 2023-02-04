e5e5ea4684c3c151e931df0adbe7c91590ba6289
Wink Saville <wink@saville.com>
2023-02-04 13:25:09 -0800

    feat: Change EchoReq and EchoReply to have timestamps'
    Use Chrono crate to get timestamp

--- echo_reply/src/lib.rs
+++ echo_reply/src/lib.rs
@@ -1,17 +1,20 @@
+use chrono::Utc;
 use msg_serde_macro::{msg_serde_macro, paste};
 
 // From: https://www.uuidgenerator.net/version4
 msg_serde_macro!(EchoReply "8206e26f-a69d-4875-8a85-0cfb636ca7c2" {
-    content: String,
-    counter: u64
+    req_timestamp_ns: i64,
+    counter: u64,
+    reply_timestamp_ns: i64
 });
 
 impl EchoReply {
-    pub fn new(content: &str, counter: u64) -> Self {
+    pub fn new(req_timestamp: i64, counter: u64) -> Self {
         Self {
             header: msg_header::MsgHeader { id: ECHO_REPLY_ID },
-            content: content.to_string(),
+            req_timestamp_ns: req_timestamp,
             counter,
+            reply_timestamp_ns: Utc::now().timestamp_nanos(),
         }
     }
 }
@@ -22,10 +25,11 @@ mod test {
 
     #[test]
     fn test_echo_reply_new() {
-        let msg = EchoReply::new("hello", 1);
+        let now_ns = Utc::now().timestamp_nanos();
+        let msg = EchoReply::new(now_ns, 1);
         println!("test_echo_reply msg={msg:?}");
         assert_eq!(msg.header.id, ECHO_REPLY_ID);
-        assert_eq!(&msg.content, "hello");
+        assert!(msg.reply_timestamp_ns >= msg.req_timestamp_ns);
         assert_eq!(msg.counter, 1);
         assert_eq!(msg.header.id.to_string(), ECHO_REPLY_ID_STR);
     }
--- echo_req/src/lib.rs
+++ echo_req/src/lib.rs
@@ -1,16 +1,17 @@
+use chrono::Utc;
 use msg_serde_macro::{msg_serde_macro, paste};
 
 // From: https://www.uuidgenerator.net/version4
 msg_serde_macro!(EchoReq "ada0f9a9-b556-46ba-b3d5-d19c87ec216c" {
-    content: String,
+    req_timestamp_ns: i64,
     counter: u64
 });
 
 impl EchoReq {
-    pub fn new(content: &str, counter: u64) -> Self {
+    pub fn new(counter: u64) -> Self {
         Self {
             header: msg_header::MsgHeader { id: ECHO_REQ_ID },
-            content: content.to_string(),
+            req_timestamp_ns: Utc::now().timestamp_nanos(),
             counter,
         }
     }
@@ -22,10 +23,12 @@ mod test {
 
     #[test]
     fn test_echo_req_new() {
-        let msg = EchoReq::new("hi", 1);
+        let now_ns = Utc::now().timestamp_nanos();
+        let msg = EchoReq::new(1);
         println!("test_echo_req msg={msg:?}");
         assert_eq!(msg.header.id, ECHO_REQ_ID);
-        assert_eq!(&msg.content, "hi");
+        // This isn't absolute true if the clock
+        assert!(msg.req_timestamp_ns >= now_ns);
         assert_eq!(msg.counter, 1);
         assert_eq!(msg.header.id.to_string(), ECHO_REQ_ID_STR);
     }
