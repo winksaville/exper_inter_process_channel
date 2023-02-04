8ef5fcfe94f4f5cf912578ae677447c69d91b5a0
Wink Saville <wink@saville.com>
2023-02-04 14:39:45 -0800

    feat: Change trait ProcessMsgAny to have a optional reply_tx
    The `reply_tx: Option<&Sender<BoxMsgAny>>` as ATM I'm not sure we'll always
    be able to have a `reply_tx`. For the time being both client and server
    support having a `partner_tx` or getting the `reply_tx` in the
    process_msg_any.

--- sm/src/lib.rs
+++ sm/src/lib.rs
@@ -1,6 +1,8 @@
+use std::sync::mpsc::Sender;
+
 use msg_header::BoxMsgAny;
 
 // Dispatch a message
 pub trait ProcessMsgAny {
-    fn process_msg_any(&mut self, msg: BoxMsgAny);
+    fn process_msg_any(&mut self, reply_tx: Option<&Sender<BoxMsgAny>>, msg: BoxMsgAny);
 }
