793a11cf3496e7433927922b53ad50c006de7136
Wink Saville <wink@saville.com>
2023-02-06 08:55:08 -0800

    feat: Rename sm to actor
    Also, added some additional types on the path to creating a `Manager`, but
    they don't do anything ATM.

--- sm/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::mpsc::Sender;
-
-use msg_header::BoxMsgAny;
-
-// Dispatch a message
-pub trait ProcessMsgAny {
-    fn process_msg_any(&mut self, reply_tx: Option<&Sender<BoxMsgAny>>, msg: BoxMsgAny);
-}
