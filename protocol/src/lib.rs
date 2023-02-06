685e3ee242945534a1e92a66d12d07e1a66e5b2a
Wink Saville <wink@saville.com>
2023-02-06 12:46:57 -0800

    feat: Create `trait Actor` and `trait Protocol`
    Change `client` and `server` to implement the Actor trait.

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,23 +1,60 @@
+use std::collections::HashMap;
+
 use msg_header::MsgId;
 use uuid::Uuid;
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct ProtocolId(pub Uuid);
 
-#[allow(unused)]
 #[derive(Clone, Debug)]
-pub struct Protocol {
-    pub name: String,
-    pub id: ProtocolId,
-    pub messages: Vec<MsgId>,
+pub struct ProtocolRec {
+    name: String,
+    id: ProtocolId,
+    protocols: Vec<ProtocolId>,
+    message_map: HashMap<ProtocolId, Vec<MsgId>>,
 }
 
-impl Protocol {
-    pub fn new(name: &str, id: ProtocolId, messages: Vec<MsgId>) -> Self {
+impl ProtocolRec {
+    pub fn new(
+        name: &str,
+        id: ProtocolId,
+        protocols: Vec<ProtocolId>,
+        message_map: HashMap<ProtocolId, Vec<MsgId>>,
+    ) -> Self {
         Self {
             name: name.to_string(),
             id,
-            messages,
+            protocols,
+            message_map,
+        }
+    }
+}
+
+pub trait Protocol {
+    fn name(&self) -> &str;
+    fn id(&self) -> &ProtocolId;
+    fn protocols(&self) -> &[ProtocolId];
+    fn messages(&self, protocol_id: &ProtocolId) -> Option<&[MsgId]>;
+}
+
+impl Protocol for ProtocolRec {
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn id(&self) -> &ProtocolId {
+        &self.id
+    }
+
+    fn protocols(&self) -> &[ProtocolId] {
+        &self.protocols
+    }
+
+    fn messages(&self, protocol_id: &ProtocolId) -> Option<&[MsgId]> {
+        if let Some(v) = self.message_map.get(protocol_id) {
+            Some(v)
+        } else {
+            None
         }
     }
 }
@@ -30,15 +67,18 @@ mod test {
     fn test_protocol() {
         println!("test_protocol");
 
-        let messages = vec![MsgId(Uuid::nil()), MsgId(Uuid::nil())];
-        let id = ProtocolId(Uuid::nil());
-        let nil_protocol = Protocol::new("nil_protocol", id, messages);
-        println!("nil_protocol={nil_protocol:?}");
-        assert_eq!(nil_protocol.name, "nil_protocol");
-        assert_eq!(nil_protocol.id.0, Uuid::nil());
-        assert_eq!(
-            nil_protocol.messages,
-            vec![MsgId(Uuid::nil()), MsgId(Uuid::nil())]
-        );
+        let messages = vec![MsgId(Uuid::new_v4())];
+        let id = ProtocolId(Uuid::new_v4());
+        let protocols = vec![id.clone()];
+        let mut message_map = HashMap::new();
+        message_map.insert(id.clone(), messages.clone());
+
+        let a_protocol = ProtocolRec::new("a_protocol", id.clone(), protocols, message_map);
+
+        println!("a_protocol={a_protocol:#?}");
+        assert_eq!(a_protocol.name(), "a_protocol");
+        assert_eq!(a_protocol.id(), &id);
+        let msgs = a_protocol.message_map.get(&id).unwrap();
+        assert_eq!(msgs, &messages);
     }
 }
