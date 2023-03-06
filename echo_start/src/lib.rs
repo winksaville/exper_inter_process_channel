bf6651bd68b1c014501c37851d707b64fa93f4fd
Wink Saville <wink@saville.com>
2023-03-06 12:44:47 -0800

    wip not compiling
    Need to implement Debug or not require Debug. Make make "not_debug" versions of
    the msg_xxx_macro's?

--- echo_start/src/lib.rs
+++ echo_start/src/lib.rs
@@ -1,9 +1,9 @@
 use actor_bi_dir_channel::ActorBiDirChannel;
-use msg_local_macro::{msg_local_macro, paste};
+use msg_local_macro::{paste, msg_local_macro_not_cloneable};
 
 // https://www.uuidgenerator.net/version4
-msg_local_macro!(EchoStart "f13f7f26-40eb-4c94-a408-c455677f6730" {
-    partner_abdc: Box<dyn ActorBiDirChannel>,
+msg_local_macro_not_cloneable!(EchoStart "f13f7f26-40eb-4c94-a408-c455677f6730" {
+    partner_abdc: Box<dyn ActorBiDirChannel + std::fmt::Debug>,
     ping_count: u64
 });
 
